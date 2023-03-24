b9893a32099e3f879936847d138c54002548291e
alaguRam <alaguram393@gmail.com>
2023-03-24 16:32:42 +0530

    update

---
+++ main.js
@@ -0,0 +1,159 @@
+    //Get current time and find(morning,afternoon,or night)
+var time=new Date();
+var ram=time.getHours();
+console.log(ram);
+
+
+if(ram<12){
+	document.write("morning");
+	}
+else if(ram>=12 && ram<=17){
+	document.write("afternoon");
+	}
+else if(ram>=18	&& ram<=20){
+	document.write("evening");
+	}	
+else{
+	document.write("night");
+}
+	
+	
+	
+	// Get current month and find(winter, fall, summer)
+// var month=new Date();
+// var save=month.getMonth();
+// console.log(save);
+
+// if(save<4){
+	// document.write("summer");
+// }
+
+// else if(ram>=4 && ram<=08){
+	// document.write("winter");
+// }
+// else{
+	// document.write("fall");
+// }
+
+
+    // Get time from user and find(morning,afternoon,or night)
+// var time=prompt("ENTER THE TIME");
+
+
+// if(time<12){
+	// document.write("morning");
+	// }
+// else if(time>=12 && time<=17){
+	// document.write("afternoon");
+	// }
+// else if(time>=18 && time<=20){
+	// document.write("evening");
+	// }	
+// else if(time>=21 && time<=24){
+	// document.write("night");
+// }
+// else{
+	// document.write("only 24 hours");
+// }
+
+
+
+   //Get month from user and find(winter, fall, summer)
+   
+   // var month=prompt("enter the month");
+   
+   // if(month<4){
+	// document.write("summer");
+	// }
+
+   // else if(month>=4 && month<=08){
+	// document.write("winter");
+	// }
+    // else if(month>=9 && month<=12){
+	// document.write("fall");
+	// }
+	// else{
+		// document.write("only 12 months");
+	// }
+	
+	
+	//Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.
+	 //  
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// function timer()
+		// {
+			// var time=new Date();
+			// console.log(time.toLocaleString());
+		// }
+	// let timeFun=setInterval(timer,2000);		
+	// function stoptime(){
+		// clearInterval(timeFun);
+	// }
